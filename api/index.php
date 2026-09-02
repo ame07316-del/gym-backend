a4f134b8813272be6840e3e19e26c4c9a801dea4
Amr Moamen <ame07316@gmail.com>
2026-09-02 19:58:02 +0300

    fix: override storage path and log channel to stderr

--- api/index.php
+++ api/index.php
@@ -1,7 +1,27 @@
 <?php
 
+// 1. فرض إعدادات البيئة في بداية التشغيل قبل تحميل Laravel
+putenv('LOG_CHANNEL=stderr');
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+putenv('SESSION_DRIVER=cookie');
+putenv('CACHE_STORE=array');
+
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['CACHE_STORE'] = 'array';
+
+// 2. إنشاء المجلدات المؤقتة داخل /tmp
+$tmpDir = '/tmp';
+@mkdir($tmpDir . '/views', 0755, true);
+@mkdir($tmpDir . '/sessions', 0755, true);
+
+// 3. تحميل الـ Autoloader والتطبيق
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 4. تغيير مسار الـ Storage ليصبح /tmp بدلاً من القرص الأساسي
+$app->useStoragePath($tmpDir);
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
